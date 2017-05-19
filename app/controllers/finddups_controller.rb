77c5a9a2e66108a1a5c66e1af7004a86ab471d0e
Rails Udemi Course <cesarprins@gmail.com>
2017-05-19 02:08:54 +0000

    Se instalo bootstrap y se actualizo el proceso para conseguir palabras repetidas

--- app/controllers/finddups_controller.rb
+++ app/controllers/finddups_controller.rb
@@ -5,31 +5,48 @@ class FinddupsController < ApplicationController
   end
   
   def result
-  @wordposition =   FindDupsSentence(params[:session][:sentence])
+    @Find_duplicated = FindDuplicatedSentence.new
+    @Find_duplicated.execute(params[:session][:sentence], params[:session][:apikey])
   end
 
 end
 
 private 
 
-def FindDupsSentence(frase)
+class FindDuplicatedSentence
 
-  sentencearray = frase.split(" ")
-  grouped_words = sentencearray.group_by { |x| x }
-  counted = grouped_words.each_with_object({}) { |(k,v), hash| hash[k] = v.count}
-  duplicated = counted.select { |k,v|  v > 1}
-  wordposition = Hash.new
-  duplicated.each do |k,v|  
-    arraydup = sentencearray.map {|x| x == k } 
-    position = []
-    indi = 0
-    arraydup.each do |bol|
-      position << indi if bol
-      indi = indi + 1
-    end
-    wordposition[k] = position 
-  end
-  wordposition
+  attr_accessor :wordposition, :code, :status 
   
-end   
+  def initialize
+    @wordposition = Hash.new
+    @code  = "200"
+    @status = "success"
+
+  end
+  
+  def execute(sentence, api_key)
+    api = Api.find_by(key: api_key)
+    if api.nil?
+      @code  = "401"
+      @status = "unauthorized"
+    else
+      sentencearray = sentence.split(" ")
+      grouped_words = sentencearray.group_by { |x| x }
+      counted = grouped_words.each_with_object({}) { |(k,v), hash| hash[k] = v.count}
+      duplicated = counted.select { |k,v|  v > 1}
+      duplicated.each do |k,v|  
+        arraydup = sentencearray.map {|x| x == k } 
+        position = []
+        indi = 0
+        arraydup.each do |bol|
+          position << indi if bol
+          indi = indi + 1
+        end
+        @wordposition[k] = position 
+      end
+    end
+      
+  end 
+  
+end
   
